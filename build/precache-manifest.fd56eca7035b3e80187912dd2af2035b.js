503703917f04b16564922995abc951d6f31df83f
adna_coder <mujibur.rehman@citiustech.com>
2019-09-02 18:25:56 +0530

    Added build folder +_+

---
+++ build/precache-manifest.fd56eca7035b3e80187912dd2af2035b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfcd5912e90dee765434cf34fc125b94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e226ef928a21a320e2d",
+    "url": "/static/css/main.4a3a7ab0.chunk.css"
+  },
+  {
+    "revision": "8f4e7bd5c00a1ee2bf82",
+    "url": "/static/js/2.e64cfc3a.chunk.js"
+  },
+  {
+    "revision": "8e226ef928a21a320e2d",
+    "url": "/static/js/main.9fc23436.chunk.js"
+  },
+  {
+    "revision": "c3a0532f97dd8842cab9",
+    "url": "/static/js/runtime~main.12f5281b.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);
